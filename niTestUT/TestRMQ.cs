109685d498a35fe3e2ba5556cac46b0cec427c22
Cyril Dumange <cdumange@gmail.com>
2019-01-29 13:49:53 +0100

    TU with consumer working.

--- niTestUT/TestRMQ.cs
+++ niTestUT/TestRMQ.cs
@@ -5,19 +5,55 @@ using ws.rmq.server.Lib;
 using NUnit.Framework;
 using Newtonsoft.Json.Linq;
 
+
 namespace niTestUT
 {
     [TestFixture]
     class TestRMQ
     {
+        RMQManager manager = null;
         string server = "192.168.1.111:5672", user = "guest", pwd = "guest";
+        string queue = "test";
+
+        [SetUp]
+        public void init()
+        {
+
+            manager = new RMQManager(server, user, pwd);
+            manager.SetQueue("test").Connect();
+        }
+
         [Test]
         public void TestEnvoiSimple()
         {
-            RMQManager manager = new RMQManager(server, user, pwd);
-            manager.SetQueue("test").Connect();
+            manager = new RMQManager(server, user, pwd);
+            manager.SetQueue(queue).Connect();
 
             Assert.True(manager.SendMessage("test"));
+        }
+        [Test]
+        public void TestListenSimple()
+        {
+            //TestContext.Out.WriteLine("test");
+            manager.SetListener(queue, callBackTest);
+        }
+
+      
+
+        public void callBackTest(string test)
+        {
+            RMQManager man = new RMQManager(server, user, pwd);
+            
+            man.SetQueue("retest").Connect().SendMessage("supertest");
+            man.Close();
+            Assert.NotNull(test);
+        }
+
+        [TearDown]
+        public void Closure()
+        {
+            if(manager.IsOpened)
+                manager.Close();
         }
     }
 }
--- ws.rmq.server/Lib/RMQManager.cs
+++ ws.rmq.server/Lib/RMQManager.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace ws.rmq.server.Lib
 {
+    /// <summary>
+    /// Class managing all processes linked to the usage of RMQ
+    /// </summary>
     public class RMQManager
     {
         public string server { get; set; }
@@ -18,6 +22,14 @@ namespace ws.rmq.server.Lib
 
         public string exchangeName { get; set; } = "Envoi vers RMQ";
 
+        public bool IsOpened { get
+            {
+                return conn != null && conn.IsOpen;
+            }
+        }
+
+        public List<EventingBasicConsumer> consumers { get; } = new List<EventingBasicConsumer>();
+
         private ConnectionFactory fact = null;
         private IConnection conn = null;
         private string connString = "amqp://{user}:{pwd}@{server}";
@@ -28,7 +40,11 @@ namespace ws.rmq.server.Lib
             this.user = user;
             this.pwd = pwd;
         }
-
+        /// <summary>
+        /// Manage the connection to the RMQ server
+        /// </summary>
+        /// <param name="forceRenew">Force the renewal of the connection</param>
+        /// <returns></returns>
         public RMQManager Connect(bool forceRenew = false)
         {
             if (conn == null || forceRenew)
@@ -65,14 +81,40 @@ namespace ws.rmq.server.Lib
             return this;
         }
 
-        public bool SendMessage(string message)
+        public RMQManager SetRouting(string routing)
         {
+            this.routing = routing;
+            return this;
+        }
+
+        /// <summary>
+        /// Verifying if connection is instanced & opened
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckConnection()
+        {
+            if (conn == null || !conn.IsOpen)
+            {
+                throw new Exception("RMQ connection not opened");
+            }
+            return true;
+        }
+        /// <summary>
+        /// Sending a message to the configured queue
+        /// </summary>
+        /// <param name="message">the string message</param>
+        /// <param name="queue">optional parameter. Allows to send a message to another queue then default</param>
+        /// <returns>was the message sent</returns>
+        public bool SendMessage(string message, string queue= null)
+        {
+            queue = queue != null ? queue : this.queue;
+            CheckConnection();
             try
             {
                 IModel model = this.conn.CreateModel();
                 model.ExchangeDeclare(this.exchangeName, ExchangeType.Direct);
-                model.QueueDeclare(this.queue, false, false, false);
-                model.QueueBind(this.queue, this.exchangeName, this.routing);
+                model.QueueDeclare(queue, false, false, false);
+                model.QueueBind(queue, this.exchangeName, this.routing);
                 model.BasicPublish(this.exchangeName, this.routing, null, Encoding.UTF8.GetBytes(message));
                 return true;
             }
@@ -83,5 +125,68 @@ namespace ws.rmq.server.Lib
             }
            
         }
+
+        public RMQManager SetListener(string queue, Action<string> callback)
+        {
+            CheckConnection();
+            IConnection conn2 = fact.CreateConnection();
+            using (IModel model = conn2.CreateModel())
+            {
+                model.ExchangeDeclare(this.exchangeName, ExchangeType.Direct);
+                model.QueueDeclare(queue, false, false, false);
+                model.QueueBind(queue, this.exchangeName, this.routing);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(model);
+                //Defining the callback
+                consumer.Received += (mod, data) =>
+                {
+                    
+                    Console.Out.WriteLine("test");
+                    string body = Encoding.UTF8.GetString(data.Body);
+                    callback(body);
+
+                };
+                //Launching the listenning
+                model.BasicConsume(queue, true, consumer);
+
+                consumers.Add(consumer);
+            }
+            
+            return this;
+        }
+
+        public RMQManager Close()
+        {
+            //Fermeture de tous les consumers
+            if (consumers != null && consumers.Count > 0)
+            {
+                try
+                {
+                    consumers.ForEach(consumer => {
+                        consumer.Model.BasicCancel(consumer.ConsumerTag);
+                    });
+                }
+                catch (Exception)
+                {
+                    //nothing to do
+                }
+                
+            }
+            //fermeture de la connection
+            if (conn != null)
+            {
+                try
+                {
+                    conn.Close();
+                }
+                catch (Exception)
+                {
+                    //nothing to do
+                }
+                
+            }
+
+            return this;
+        }
+
     }
 }
